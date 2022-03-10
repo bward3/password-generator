d03fc81c138276d4d64f3ff0c3ddff38870818d0
Brian Ward <brianwardcs@gmail.com>
2022-03-10 15:12:15 -0700

    Connected input sequence to generate password button

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordLength = prompt("Enter the desired length of your password. Must be between 8 and 128 characters.");
-var lengthVerify = false;
-passwordLength = parseInt(passwordLength);
-while (!lengthVerify) {
-  if (!(8 <= passwordLength <= 128)) {
-    lengthVerify = true;
-  } else {
-    passwordLength = prompt("Invalid input. Please enter a number between 8 and 128.")
-  }
-}
 
 
 // Write password to the #password input
 function writePassword() {
+  var passwordLength = prompt("Enter the desired length of your password. Must be between 8 and 128.");
+  var lengthVerify = false;
+  passwordLength = parseInt(passwordLength);
+
+  while (!lengthVerify) {
+    if (8 <= passwordLength && passwordLength <= 128) {
+      lengthVerify = true;
+    } else {
+      passwordLength = prompt("Invalid input. Please enter a number between 8 and 128.")
+    }
+  }
+  var criteriaVerify = false;
+
+  var includeLower = confirm("Include lowercase?");
+  while (!criteriaVerify) {
+    var includeUpper = confirm("Include uppercase?");
+    var includeSymbol = confirm("Include special characters?");
+    var includeNumbers = confirm("Include numbers?");
+    if (includeLower || includeUpper || includeSymbol || includeNumbers) {
+      criteriaVerify = true;
+    } else {
+      includeLower = confirm("Need at least 1 criteria. Include lowercase?")
+    }
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
