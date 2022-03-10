9a4eba16d8766fcaa979cda45dc88ca03d64a71f
Brian Ward <brianwardcs@gmail.com>
2022-03-10 14:08:29 -0700

    Added initial prompt

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordLength = prompt("Enter the desired length of your password. Must be between 8 and 128 characters.");
+var lengthVerify = false;
+passwordLength = parseInt(passwordLength);
+while (!lengthVerify) {
+  if (!(8 <= passwordLength <= 128)) {
+    lengthVerify = true;
+  } else {
+    passwordLength = prompt("Invalid input. Please enter a number between 8 and 128.")
+  }
+}
+
 
 // Write password to the #password input
 function writePassword() {
